8c13cb142c842258b6e0c7ee0f90b104e6a6d107
Long6723 <long06072003@gmail.com>
2026-04-08 15:13:43 +0700

    feat: initial commit

---
+++ app/components/home-page.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import { Container } from "@mui/material";
+
+export default function HomePage() {
+  return (
+    <div className="home-page">
+        <div className="home-page__container">
+            <Container>
+                <div></div>
+            </Container>
+        </div>
+    </div>
+  );
+}
